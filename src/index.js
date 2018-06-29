92e0bbdf3579487fb9b1611487dd8c46f1a9026f
adsellor <adsellor@protonmail.com>
2018-06-29 21:10:06 +0400

    add cloned nodes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,38 @@ const mappedImages = imageData.images.map((index) => index) // Google mdn Array.
 // Create a new <img> element for each member of array, return <img> element with the src="array[index]"
 // Additional: change the html with the new <img src="array[index]"
 //
+//
 
-function sortImages(array) {
-    let newArray = array.map((element) => {
+function cloneNode(node) {
+    return node.cloneNode(true)
+}
+
+function sortImages(data) {
+    let newArray = data.images.map((element) => {
         let image = document.createElement("img")
+        let headerImage = document.createElement("img")
         let attr = document.createAttribute('src')
+        let classAttr = document.createAttribute('class')
+        classAttr.value = 'footerimage'
         attr.value = element
-        let wrapper = document.getElementById('image-wrapper')
         image.setAttributeNode(attr)
-        //wrapper.appendChild(image)
-        console.log(wrapper)
-        return image
+        image.setAttributeNode(classAttr)
+
+        //console.log(image)
+        let root = document.getElementById('root')
+        let clonedNode = cloneNode(root)
+        for(let elem of clonedNode.children) {
+            let attr = elem.getAttribute('id')
+            if(attr == 'image-wrapper') {
+                let imageElem = elem
+                imageElem.appendChild(image)
+                //console.log(imageElem)
+            }
+        }
+        //console.log(clonedNode.children)
+        document.body.appendChild(clonedNode)
     })
 }
 
-console.log(sortImages(imageData.images))
+console.log(sortImages(imageData))
+
